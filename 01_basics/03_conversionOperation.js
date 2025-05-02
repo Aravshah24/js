6586f34c16610bbbad6b8e2031992c00fc5c3b55
Aravshah24 <aravshah2411@gmail.com>
2025-05-02 06:00:31 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33aa"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" -- to 33
+// "33abc" -- NaN
+// true -- 1 ; false -- 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" -- false
+// "abc" -- true
+
+let num = 33
+let stringnum = String(num)
+console.log(stringnum);
+console.log(typeof stringnum);
+
+
