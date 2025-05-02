86f45f0120b12726a3c8ff2c8b27ce448f825ff4
Aravshah24 <aravshah2411@gmail.com>
2025-05-02 07:57:25 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+//console.log(2>1);
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// ===  strictly checks even for datatypes
+console.log("2"===2);
+
+//does conversion and works
+console.log("2"==2);
+
+
