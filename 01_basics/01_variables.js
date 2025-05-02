b1dda06fd175c7c75f8edb899f6b7fcef2ee1450
Aravshah24 <aravshah2411@gmail.com>
2025-05-02 05:44:01 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144523
+let accountEmail = "arav@gmail.com"
+var accountPassword = "1234"
+accountCity = "Hubli"
+let accountState
+
+// accountId = 12
+
+accountEmail = "abcd@gmail.com"
+accountPassword = "21212112"
+accountCity = "Bangalore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; // used for newer version of js
+
+//alert(3+3) //doesn't work on nodejs, works on browser
+
+
+let name = "arav"
+let age = 20
+let isLoggedin = false
+
+
